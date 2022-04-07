b27f8dc687c560922c513345492264e9009b27c1
ohmkrishnasarang <93313136+ohmkrishnasarang@users.noreply.github.com>
2022-04-07 14:47:09 +0530

    commit

---
+++ intfloat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class intfloat
+{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter integer: ");
+		int a;
+		a = sc.nextInt();
+		System.out.println("Enter float: ");
+		float b = sc.nextFloat();
+
+		System.out.println("Integer: " + a + "\nFloat: " + b );
+	}
+}
